39e07caa5291186f39979f3ea674951c59425a9c
Xemno <elokaili.qais@gmail.com>
2025-09-23 00:12:09 +0200

    enable prerender

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const base = '/portfolio-svelte';
+const dev = process.argv.includes('dev');
+const base = dev ? '' : '/portfolio-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,8 +10,33 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({ fallback: '404.html' }),
-
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html' 
+		}),
+		prerender: {
+			// use relative URLs similar to an anchor tag <a href="/test/1"></a>
+			// do not include group layout folders in the path such as /(group)/test/1
+			entries: [
+				'/',
+				'/education',
+				'/experience',
+				'/projects',
+				'/search',
+				'/skills',
+				'/projects/my-portfolio-website',
+				'/projects/dr1ve-website',
+				'/projects/master-thesis',
+				'/projects/bachelor-thesis',
+				'/projects/mr-lab-eth',
+				'/projects/fast-cpu-raymarcher',
+				'/projects/soft-body-physics-engine',
+				'/projects/ai-road-segmentation',
+				'/experience/qualitas-ag',
+				'/experience/urban-games'
+			]
+		},
 		alias: {
 			$lib: './src/lib',
 			'@data': './src/lib/data',
@@ -21,7 +47,7 @@ const config = {
 			'@utils': './src/lib/utils'
 		},
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? base : ''
+			base: base
 		}
 	}
 };
